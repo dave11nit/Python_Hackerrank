5d30c1b0dcc388c94f44650e05eac4b676a8a953
Devayan Mishra <66617952+dave11nit@users.noreply.github.com>
2021-09-07 12:34:39 +0530

    Add files via upload

---
+++ Arithematic_operators.py
@@ -0,0 +1,12 @@
+def arithematic_operator(x,y):
+    print(x+y)
+    print(x-y) 
+    # we need to calculate the difference of first - second
+    # if we wanted the absolute value 
+    # print(abs(x-y))
+    print(x*y)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    arithematic_operator(a,b)
