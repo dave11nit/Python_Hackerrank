58255f312ff2145973088d0218343601ce6287de
Devayan Mishra <66617952+dave11nit@users.noreply.github.com>
2021-09-07 12:56:10 +0530

    Add files via upload
    You receive your first star after this program

---
+++ Python_division.py
@@ -0,0 +1,8 @@
+def division(x,y):
+    print(x//y) # this is floor division
+    print(x/y)  # this is normal division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    division(a,b)
